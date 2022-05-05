e776b9e7ebd3a3833a03e2efe594b134ea9d8c7f
Sharoon Thomas <sharoon.thomas@fulfil.io>
2022-05-05 19:58:24 -0400

    Blackify. Deprecate py2 support

--- setup.py
+++ setup.py
@@ -4,53 +4,52 @@
 
 from setuptools import find_packages, setup
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.md') as history_file:
+with open("HISTORY.md") as history_file:
     history = history_file.read()
 
 requirements = [
-    'requests',
-
-    # Drop these when py2 support is dropped
-    'future',
-    'six',
+    "requests",
 ]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="Fulfil.IO Inc.",
-    author_email='help@fulfil.io',
-    python_requires='>=2.7',
+    author_email="help@fulfil.io",
+    python_requires=">=3.7",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 2.7',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     description="Python utility to encode/decode GS1 EPCs",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
-    long_description_content_type='text/markdown',
+    long_description=readme + "\n\n" + history,
+    long_description_content_type="text/markdown",
     include_package_data=True,
-    keywords='pyepc epc',
-    name='pyepc',
-    packages=find_packages(include=['pyepc', 'pyepc.*']),
+    keywords="pyepc epc",
+    name="pyepc",
+    packages=find_packages(include=["pyepc", "pyepc.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/fulfilio/pyepc',
-    version='0.0.3',
+    url="https://github.com/fulfilio/pyepc",
+    version="0.4.0",
     zip_safe=False,
 )
